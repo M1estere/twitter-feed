ba8c70e17e6c85b3eab260a85ad3606d94f8bec1
Ilya_Solov <7156643@gmail.com>
2024-04-12 19:53:26 +0300

    Implemented authorization logic (excluding sql injections)

--- auth.php
+++ auth.php
@@ -14,8 +14,45 @@
     <?php
         session_start();
         require './server/db_connection.php';
+    ?>
 
-        require './server/get_posts.php';
+    <?php
+        require './server/auth.php';
+
+        $error = '';
+
+        if (isset($_GET['type'])) {
+            $type = $_GET['type'];
+
+            if ($type == 'login') {
+                if (isset($_GET['user_nickname']) && isset($_GET['user_password'])) {
+                    $response = login($_GET['user_nickname'], $_GET['user_password']);
+                    if ($response == -2) {
+                        $error = 'User does not exist!';
+                    } else if ($response == -1) {
+                        $error = 'Wrond password';
+                    } else if ($response == 0) {
+                        // header('Location:./index.php');
+                    } else {
+                        $error = 'Some error occured';
+                    }
+                }
+            } else if ($type == 'reg') {
+                if (isset($_GET['user_nickname']) && isset($_GET['user_name']) && isset($_GET['user_password'])) {
+                    $response = register($_GET['user_nickname'], $_GET['user_name'], $_GET['user_password']);
+                    
+                    if ($response == -2) {
+                        $error = 'User already exists';
+                    } else if ($response == -1) {
+                        $error = 'Error adding a new user';
+                    } else if ($response == 0) {
+                        // header('Location:./index.php');
+                    } else {
+                        $error = 'Some error occured';
+                    }
+                }
+            }
+        }        
     ?>
 
     <main>
@@ -23,27 +60,36 @@
             <div id="header" class="h-[25px] bg-blue-500 w-full">
             </div>
 
-            <div class="flex-1 w-full h-full flex justify-center items-center">
-                <div id="central-block" class="w-[500px] h-[650px] rounded-[15px] border-[1px] border-[var(--main-black)] shadow-lg px-[15px] py-[20px] flex flex-col gap-y-[30px]">
+            <div class="flex-1 w-full h-full flex flex-col justify-center gap-y-[10px] items-center">
+                <div>
+                    <?php
+                        echo "<span class='font-normal text-lg uppercase text-red-500'>{$error}</span>";
+                    ?>
+                </div>
+                <div id="central-block" class="w-[350px] h-[500px] rounded-[15px] border-[1px] border-[var(--main-black)] shadow-lg px-[15px] py-[20px] flex flex-col gap-y-[30px]">
                     <div class="flex flex-row justify-center items-center w-full h-fit gap-x-[20px]">
                         <span id="login-button" class='text-lg text-[var(--main-black)] font-bold uppercase border-[var(--main-black)] border-b-2 cursor-pointer'>Login</span>
                         <span id="reg-button" class='text-lg text-[var(--main-black)] font-bold uppercase border-[var(--main-black)] cursor-pointer'>Register</span>
                     </div>
 
                     <div id="login-form" class="size-full visible">
-                        <form method="get" action="./index.php" class="size-full flex flex-col justify-start items-center gap-y-[5px]">
-                            <input type='text' name='user_nickname' placeholder="Enter your nickname..." class="w-full h-[50px] p-[5px] font-normal text-lg rounded-[10px] focus:outline-none" />
-                            <input type='password' name='user_password' placeholder="Enter your password..." class="w-full h-[50px] p-[5px] font-normal text-lg rounded-[10px] focus:outline-none" />
+                        <form method="get" action="#" class="size-full flex flex-col justify-start items-center gap-y-[5px]">
+                            <input type='text' name='type' value='login' hidden />
+
+                            <input type='text' name='user_nickname' placeholder="Enter your nickname..." class="w-full h-[50px] p-[5px] font-normal text-lg rounded-[10px] focus:outline-none" required />
+                            <input type='password' name='user_password' placeholder="Enter your password..." class="w-full h-[50px] p-[5px] font-normal text-lg rounded-[10px] focus:outline-none" required />
                         
                             <input class="bg-blue-500 rounded-[10px] h-[35px] w-full text-lg text-[var(--main-white)] font-normal uppercase cursor-pointer" type="submit" value="Login" />
                         </form>
                     </div>
 
                     <div id="reg-form" class="size-full hidden">
-                        <form method="get" action="./index.php" class="size-full flex flex-col justify-start items-center gap-y-[5px]">
-                            <input type='text' name='user_nickname' placeholder="Enter your nickname..." class="w-full h-[50px] p-[5px] font-normal text-lg rounded-[10px] focus:outline-none" />
-                            <input type='text' name='user_name' placeholder="Enter your name..." class="w-full h-[50px] p-[5px] font-normal text-lg rounded-[10px] focus:outline-none" />
-                            <input type='password' name='user_password' placeholder="Enter your password..." class="w-full h-[50px] p-[5px] font-normal text-lg rounded-[10px] focus:outline-none" />
+                        <form method="get" action="#" class="size-full flex flex-col justify-start items-center gap-y-[5px]">
+                            <input type='text' name='type' value='reg' hidden />
+                        
+                            <input type='text' name='user_nickname' placeholder="Enter your nickname..." class="w-full h-[50px] p-[5px] font-normal text-lg rounded-[10px] focus:outline-none" required />
+                            <input type='text' name='user_name' placeholder="Enter your name..." class="w-full h-[50px] p-[5px] font-normal text-lg rounded-[10px] focus:outline-none" required />
+                            <input type='password' name='user_password' placeholder="Enter your password..." class="w-full h-[50px] p-[5px] font-normal text-lg rounded-[10px] focus:outline-none" required />
                         
                             <input class="bg-blue-500 rounded-[10px] h-[35px] w-full text-lg text-[var(--main-white)] font-normal uppercase cursor-pointer" type="submit" value="Register" />
                         </form>
---
+++ server/auth.php
@@ -0,0 +1,64 @@
+<?php
+
+    function login($nickname, $password) {
+        global $server_connection;
+
+        $nickname = trim($nickname);
+        $password = trim($password);
+
+        $user = userExists($nickname);
+        if ($user == null) {
+            return -2;
+        } else {
+            if ($user['password'] == $password) {
+                return 0;
+            } else {
+                return -1;
+            }
+        }
+    }
+
+    function register($nickname, $name, $password) {
+        global $server_connection;
+
+        $nickname = trim($nickname);
+        $name = trim($name);
+        $password = trim($password);
+
+        $user = userExists($nickname);
+        if ($user != null) {
+            return -2;
+        } else {
+            $date = date('Y-m-d', time());
+            $stmt = $server_connection->prepare("INSERT INTO users (nickname, name, reg_date, password) VALUES (?, ?, '$date', ?);");
+            $stmt->bind_param('sss', $nickname, $name, $password);
+
+            if ($stmt->execute()) {
+                $stmt->close();
+                return 0;
+            } else {
+                $stmt->close();
+                return -1;
+            }
+        }
+    }
+
+    function userExists($nickname) {
+        global $server_connection;
+
+        $stmt = $server_connection->prepare('SELECT * FROM users WHERE nickname = ?;');
+        $stmt->bind_param('s', $nickname);
+        if ($stmt->execute()) {
+            $result = $stmt->get_result();
+            $stmt->close();
+            if ($result == null) return null;
+            
+            while ($user = $result->fetch_assoc()) {
+                return $user;
+            }
+        } else {
+            return null;
+        }
+    }
+
+?>
